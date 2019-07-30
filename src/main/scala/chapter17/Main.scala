bc7d9fca2f993c52b19d579010e0534dd7cf78ee
mikhail <mikhail_shabarov@mail.ru>
2019-07-30 11:29:38 +0300

    Chapter17: ex05-06 added

--- src/main/scala/chapter17/Main.scala
+++ src/main/scala/chapter17/Main.scala
@@ -2,7 +2,7 @@ package chapter17
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Random, Success}
 
 object Main extends App {
 
@@ -16,7 +16,13 @@ object Main extends App {
     case Failure(exception) => println(exception.getMessage)
   }
 
-  for (n1 <- Future { Thread.sleep(1000); 2 }; n2 <- Future { Thread.sleep(1000); 40}) {
+  for (n1 <- Future {
+    Thread.sleep(1000);
+    2
+  }; n2 <- Future {
+    Thread.sleep(1000);
+    40
+  }) {
     println("Future sum result = " + (n1 + n2))
   }
 
@@ -43,34 +49,82 @@ object Main extends App {
   }
 
   //ex02
-  def f_(x : Int) = Future[String] { "" + x + "." + x }
-  def g_(y : String) = Future[Double] { y.toDouble }
+  def f_(x: Int) = Future[String] {
+    "" + x + "." + x
+  }
+
+  def g_(y: String) = Future[Double] {
+    y.toDouble
+  }
 
   val g_compose_f = doInOrder(f_, g_)
   g_compose_f(1).foreach(n => println(s"Composed result = ${n}"))
 
   doSomeWork()
 
-  def doInOrder[T, U, V](f : (T) => Future[U], g : U => Future[V]) : (T) => Future[V] = {
+  def doInOrder[T, U, V](f: (T) => Future[U], g: U => Future[V]): (T) => Future[V] = {
     (x: T) => {
       f(x) flatMap (q => g(q))
     }
   }
 
   //ex03
-  def f3(x : String) = Future { x + "one! " }
-  def f4(x : String) = Future { x + "two! " }
-  def f5(x : String) = Future { x + "three!" }
+  def f3(x: String) = Future {
+    x + "one! "
+  }
+
+  def f4(x: String) = Future {
+    x + "two! "
+  }
+
+  def f5(x: String) = Future {
+    x + "three!"
+  }
+
   val compose_multiple = doInOrder2(f3, f4, f5)
   compose_multiple("start! ").foreach(n => println(s"Multiple composed result = ${n}"))
 
   doSomeWork()
 
-  def doInOrder2[T](r: (T) => Future[T], f : ((T) => Future[T])*) : (T) => Future[T] = {
-    (x : T) => {
+  def doInOrder2[T](r: (T) => Future[T], f: ((T) => Future[T])*): (T) => Future[T] = {
+    (x: T) => {
       var t = r(x)
       for (u <- f) t = t.flatMap(n1 => u(n1))
       t
+    }
+  }
+
+  //ex05
+  def f6(x: Int) = Future {
+    x
+  }
+
+  def f7(x: Int) = Future {
+    x
+  }
+
+  getFutureSequence(Seq(f6(1), f7(2))).foreach(n => println(s"Future sequence = ${n}"))
+
+  def getFutureSequence[T](f: Seq[Future[T]]): Future[Seq[T]] = {
+    Future.sequence(f)
+  }
+
+  //ex06
+  val r = new Random()
+  val repeatResult = repeat(() => {
+    val next = r.nextInt(10); println(s"Next Int = ${next}"); Thread.sleep(1000); next
+  }, (t: Int) => t > 5)
+  repeatResult.foreach(n => println("Repeat res = " + n))
+
+  doSomeWork()
+
+  def repeat[T](action: () => T, until: T => Boolean): Future[T] = {
+    Future {
+      var res: T = null.asInstanceOf[T]
+      while (!until({
+        res = action(); res
+      })) {}
+      res
     }
   }
 
